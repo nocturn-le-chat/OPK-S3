da8294d2cbcb11d227a84d1b248da25f6ca78519
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-09-09 14:59:05 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(double a, b, c) {
-  printf("A >>> \n B >>> \n; C >>> \n"); scanf("%d", &a); scanf("%d", &b); scanf("%d", &c);
-  if a == 0: return c / b;
-  if b == 0 and c > 0: return sqrt(-a);
-  if b == 0 and c < 0: return sqrt(a);
-  if (a == 0 and c == 0) or (b == 0 and c == 0): return 0;
-  if 
+int main() {
+    double a, b, c;
+    double epsilon = 0.001;
+    printf("Enter numbers comma-separated: "); scanf("%lf, %lf, %lf", a, b, c);
+    if (fabs(a) < epsilon) {printf(c/b);};
+    if (b==0 && c<0)  {printf("x = %lf", sqrt(c/a));};
+    if (b==0 && c==0) {printf("x = 0");};
+    if (b==0 && c>0)  {printf("x = %lf", sqrt(-c/a));};
+    if (a==0 && c==0) {printf("x = 0");};
+    if (a!=0 && b!=0 && c!=0) {
+        double d = pow(b, 2) - 4*a*c;
+        double res1 = (-b + sqrt(d))/(2*a);
+        double res2 = (-b - sqrt(d))/(2*a);
+        printf("%lf, %lf", res1, res2); 
+    };
 }
