b5fbfd01bb12f30c82b6f5ec6d8e5f2cc570910d
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-09-09 13:39:08 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
-static void main {}
+#include <stdio.h>
+#include <math.h>
+
+int main(double a, b, c) {
+  printf("A >>> \n B >>> \n; C >>> \n"); scanf("%d", &a); scanf("%d", &b); scanf("%d", &c);
+  if a == 0: return c / b;
+  if b == 0 and c > 0: return sqrt(-a);
+  if b == 0 and c < 0: return sqrt(a);
+  if (a == 0 and c == 0) or (b == 0 and c == 0): return 0;
+  if 
+}
