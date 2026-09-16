53e3e0ed8c7f8171e8ea896ff819d51510e5f991
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-09-16 15:01:39 +0700

    Minor optimization and debugging

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 
-int main() {
-    printf("Input range: >>> "); int range; scanf("%d", range);    //Ввод данных и создание массива
-    int arr[range];
+void main() {
+    printf("Input range: >>> ");                                   
+    int range; scanf("%d", &range); bool array[range+1];
 
-    for (int i = 0; i < range; i++) {arr[i] = i + 2;};             //Наполнение массива числами
+    for (int i = 0; i < range; i++) {array[i] = true;};
+    array[0] = false; array[1] = false;                                       
 
-    int volume = 1.25506*(range/log(range));
-    int primes[volume];                                            //Массив для простых чисел
-
-
-    for (int i = 0; i < range; i++) {                              //"Решето Эратосфена"
-        if (arr[i] != 0) {
-            int value = arr[i];                                    //Берется ненулевой элемент массива...
+    for (int i = 0; i < range; i++) {                          
+        if (array[i] != 0) {
+            int value = i;                               
             for (int j = i+1; j < range; j++) {
-                if ((arr[j]!="NULL")&&(arr[j]%value == 0)) {
-                    arr[j]= "NULL";                               //... и все кратные ему заменяются на NULL
+                if ((array[j]!=false)&&(j%value == 0)) {
+                     array[j]= false;
                 }
-            printf("%d", value);
             }
-        }
+            printf("%d\n", value);
+        } 
     }
 }
