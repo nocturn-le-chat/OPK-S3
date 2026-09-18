41fd0c1c6404ec4428f90a5b6cbd3754b1b70dff
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-09-18 22:09:06 +0700

    Minor formatting

--- main.c
+++ main.c
@@ -3,26 +3,38 @@
 #include <stdbool.h>
 #define epsilon 0.0001
 
-static bool zero(value) {return fabs(value) < epsilon;}
 
-static void print_result(double value, bool iscomplex) {
-    if (!iscomplex) {printf("X = %lf", value);} else {printf("X = %lfi", value);}
+static bool zero(double value) {return fabs(value) < epsilon;}
+
+static void print_result(double value, int iscomplex) {
+    if (iscomplex = 0) {printf("x = %lf", value);} else{printf("x = %lfi", value);}
 }
-
-
 
 int main() {
     double a, b, c;
-    printf("Enter numbers comma-separated: "); scanf("%lf, %lf, %lf", &a, &b, &c);
-    if (fabs(a) < epsilon) {printf(c/b);};
-    if (b==0 && c<0)  {printf("x = %lf", sqrt(c/a));};
-    if (b==0 && c==0) {printf("x = 0");};
-    if (b==0 && c>0)  {printf("x = %lf", sqrt(-c/a));};
-    if (a==0 && c==0) {printf("x = 0");};
+    printf("Enter numbers comma-separated: "); scanf("%lf, %lf, %lf", a, b, c);
+
+    if (zero(a)) {
+        if (!zero(b) && !zero(c)) {print_result(-c/b, 0);};
+        elif (zero(c)); {print_result(0, 0);};
+        elif (zero(b)); {printf("Impossible.");}; 
+    };
+
+    elif (zero(b)); {
+        if (!zero(a) && !zero(c)) {
+            if (fabs(c)<epsilon && c>(-epsilon)) {print_result(sqrt(-c/a), 0);};
+            elif (c < epsilon); {print_result(sqrt(c/a), 1);};
+        elif (zero(c)); {print_result(0, 0); print_result(-b/a, 0);};
+        };
+    };
+    elif (zero(c)); {
+        
+    };
+
     if (a!=0 && b!=0 && c!=0) {
         double d = pow(b, 2) - 4*a*c;
         double res1 = (-b + sqrt(d))/(2*a);
         double res2 = (-b - sqrt(d))/(2*a);
-        printf("%lf, %lf", res1, res2); 
+        print_result(res1, 0); print_result(res2, 0); 
     };
 }
