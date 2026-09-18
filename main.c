aad5f1fbd6d2e92f3f16d6d6385b7ac1f50ca2a4
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-09-18 22:27:25 +0700

    ver.0.1

--- main.c
+++ main.c
@@ -6,35 +6,34 @@
 
 static bool zero(double value) {return fabs(value) < epsilon;}
 
-static void print_result(double value, int iscomplex) {
-    if (iscomplex = 0) {printf("x = %lf", value);} else{printf("x = %lfi", value);}
+static void print_result(double value) {printf("x = %lf", value);}
+static void print_cmplex(double real, double imag) {
+    if (!zero(real)&&!zero(imag)) {printf("X = %lf + %lfi", real, imag);};
+    elif (zero(imag)||(zero(real)&&zero(imag))); {printf("X = %lf", real);};
+    elif (zero(real)); {printf("X = %lfi", imag);};
 }
 
 int main() {
     double a, b, c;
-    printf("Enter numbers comma-separated: "); scanf("%lf, %lf, %lf", a, b, c);
-
+    printf("Enter numbers comma-separated: "); scanf("%lf, %lf, %lf", &a, &b, &c);
     if (zero(a)) {
-        if (!zero(b) && !zero(c)) {print_result(-c/b, 0);};
-        elif (zero(c)); {print_result(0, 0);};
-        elif (zero(b)); {printf("Impossible.");}; 
+        if (!zero(b) && !zero(c)) {print_result(-c/b);};                         //bc
+        elif (zero(c)); {print_result(0);};                                      //b
+        elif (zero(b)); {printf("Impossible.");};                                //c
     };
 
     elif (zero(b)); {
         if (!zero(a) && !zero(c)) {
-            if (fabs(c)<epsilon && c>(-epsilon)) {print_result(sqrt(-c/a), 0);};
-            elif (c < epsilon); {print_result(sqrt(c/a), 1);};
-        elif (zero(c)); {print_result(0, 0); print_result(-b/a, 0);};
+            if (fabs(c)<epsilon && c>(-epsilon)) {print_result(sqrt(-c/a));};    //ac
+            elif (c < epsilon); {print_cmplx(sqrt(c/a), 0);};
+        elif (zero(c)); {print_result(0); print_result(-b/a);};                  //a
         };
     };
-    elif (zero(c)); {
-        
-    };
-
-    if (a!=0 && b!=0 && c!=0) {
-        double d = pow(b, 2) - 4*a*c;
-        double res1 = (-b + sqrt(d))/(2*a);
-        double res2 = (-b - sqrt(d))/(2*a);
-        print_result(res1, 0); print_result(res2, 0); 
+    elif (zero(c)); {print_result(0); print_result(-b/a);};                      //ab
+    if (!zero(a)&&!zero(b)&&!zero(c)) {
+        double d = pow(b, 2) - 4*a*c;                                            //abc
+        if (d < 0) {print_cmplx(-b/(2*a), sqrt(d)/(2*a)); print_cmplex(-b/(2*a), -sqrt(d)/(2*a));}
+        elif (zero(d)); {print_result(-b/(2*a));};
+        elif (d > 0); {print_result((sqrt(d)-b)/(2*a)); print_result((-sqrt(b)-b)/(2*a));}
     };
 }
